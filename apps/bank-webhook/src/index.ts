8f047238ad01f507356fc43c47b3d33d556e9953
Navkirat singh <132665095+Vanillapapa64@users.noreply.github.com>
2025-07-15 22:47:01 +0530

    minor changes

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -43,7 +43,7 @@ app.post("/hdfcWebhook", async (req, res) => {
     } catch(e) {
         console.error(e);
         res.status(411).json({
-            message: "Error while processing webhook"
+            message: "error aa gyi"
         })
     }
 
